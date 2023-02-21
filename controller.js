7b056c049ff06b086c8996f0336c925c59b9f69d
TubbieTubTub <derekphung16@gmail.com>
2023-02-20 22:59:39 -0500

    something++

--- controller.js
+++ controller.js
@@ -8,9 +8,23 @@
 
 
 /**
- * name: Deric
+ * name: Derek
  * date: 2/20/2023
  */
+
+class CardCreation{
+  age;
+  place;
+  name;
+    constructor(userAge, userPlace, userName){
+      this.age = userAge;
+      this.place = userPlace;
+      this.name = userName;
+    }
+  }
+
+
+
 let cardList = [
   {
      age: 2,
@@ -21,11 +35,19 @@ let cardList = [
   {
       age: 16,
       place: "2nd",
-      name: "Deric" 
-   }
+      name: "Derek" 
+   },
+
+   new CardCreation(10000, "3rd", "Diablo"),
+   
+   2
 ];
 
+
+
 function webPage(){
+
+  document.getElementById("bigBox").innerHTML = ``;
 
   for(let i = 0; i < cardList.length; i++){
       document.getElementById("bigBox").innerHTML += `
@@ -37,6 +59,41 @@ function webPage(){
   `;
   }
 
+  document.getElementById("bigBox").innerHTML += `
+    <button id="button" onclick="formCreation()">+</button>
+  `;
+
+}
+
+function formCreation(){
+  document.getElementById("bigBox").innerHTML += `
+  <div id="formBox">
+    <div>
+      <button id="formButton" onclick="webPage()">X</button>
+    </div>
+    <div id="formDiv">
+      <div id="formAge">Age:<input type="number" id="userAge"></div>
+      <div id="formPlace">Place:<input type="text" id="userPlace"></div>
+      <div id="formName">Name:<input type="text" id="userName"></div>
+      <button onclick="pushToArray()">Submit</button>
+    </div>
+    
+  </div>
+
+  
+  `;
+}
+
+function pushToArray(){
+
+  // cardList.push(userAge.value);
+  
+  let age = document.getElementById("userAge").value;
+  let place = document.getElementById("userPlace").value;
+  let name = document.getElementById("userName").value;
+
+  cardList.push(new CardCreation(age, place, name));
+  webPage();
 }
 
 
